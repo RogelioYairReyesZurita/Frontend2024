4243904cb1371c10f9e48ccd9072e50e014faf4e
RogelioYairReyesZurita <reyesryz2002@gmail.com>
2024-05-31 22:59:12 -0600

    trabajoF

--- 11_Trabajo-Final/src/CountrySearch.jsx
+++ 11_Trabajo-Final/src/CountrySearch.jsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const CountrySearch = () => {
-  const [searchType, setSearchType] = useState([]);
+  const [searchType, setSearchType] = useState('');
   const [searchTerm, setSearchTerm] = useState('');
-  const [countryData, setCountryData] = useState(null);
+  const [countryDataList, setCountryDataList] = useState([]);
 
+
+  // ------------------------pagina handleSearch ------------------------------
   const handleSearch = async () => {
     try {
       let response;
@@ -16,7 +18,7 @@ const CountrySearch = () => {
       } else if (searchType === 'capital') {
         response = await axios.get(`https://restcountries.com/v3.1/capital/${searchTerm}`);
       }
-      setCountryData(response.data[0]);
+      setCountryDataList([...countryDataList, response.data[0]]);
     } catch (error) {
       console.error('Error fetching country data:', error);
       setCountryData(null);
@@ -24,13 +26,11 @@ const CountrySearch = () => {
   };
 
 
-
-
+  // -----------------------------------------------------------------------
   const handleButtonClick = (type) => {
     setSearchType(type);
     setSearchTerm('');
   };
-
   const handleInputChange = (e) => {
     setSearchTerm(e.target.value);
   };
@@ -39,15 +39,8 @@ const CountrySearch = () => {
 
   return (
     <div >
-
-      {/* <div>
-        <button onClick={() => handleButtonClick('name')}>Buscar por Nombre</button>
-        <button onClick={() => handleButtonClick('language')}>Buscar por Lenguaje</button>
-        <button onClick={() => handleButtonClick('capital')}>Buscar por Capital</button>
-      </div> */}
-
       <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
-
+        {/* --------------Botones------------------------------ */}
         <input
           type="radio"
           className="btn-check"
@@ -87,20 +80,9 @@ const CountrySearch = () => {
         >Buscar por Capital</label>
       </div>
 
-
-     
-      {/* <div className="input-group mb-3">
-        <input
-          className="form-control w-50"
-          type="text"
-          placeholder={searchType === 'language' ? 'Ingrese código de lenguaje (por ej., en, es)' : 'Ingrese término de búsqueda'}
-          value={searchTerm}
-          onChange={handleInputChange}
-        />
-        
-      </div> */}
-     <br />
-     <br />
+      {/* ------------------------------------busquedas----------------------- */}
+      <br />
+      <br />
       <div>
         <div className="row">
           <div className="col-md-6 ml-0">
@@ -124,43 +106,21 @@ const CountrySearch = () => {
         </div>
       </div>
 
-
-      {countryData && (
-
-        // <table>
-        //   <thead>
-        //     <tr>
-        //       <th>Name</th>
-        //       <th>Capital</th>
-        //       <th>Population</th>
-        //       <th>Area</th>
-        //       <th>Flag</th>
-        //     </tr>
-        //   </thead>
-        //   <tbody>
-        //     <tr>
-        //       <td>{countryData.name.common}</td>
-        //       <td>{countryData.capital}</td>
-        //       <td>{countryData.population}</td>
-        //       <td>{countryData.area} km²</td>
-        //       <td>
-        //         <img src={countryData.flags.svg} alt={`${countryData.name.common} flag`} height="50" />
-        //       </td>
-        //     </tr>
-        //   </tbody>
-        // </table>
-
-        // ----------------------------------------------------------------------
-        <div className="card" style={{ width: '18rem' }}>
-        <img src={countryData.flags.svg} alt={`${countryData.name.common} flag`} class="card-img-top"/>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">Name: {countryData.name.common}</li>
-          <li className="list-group-item">Capital: {countryData.capital}</li>
-          <li className="list-group-item">Population: {countryData.population}</li>
-          <li className="list-group-item">Area: {countryData.area} km²</li>
-        </ul>
+      {/* ------------card-------------------------- */}
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {countryDataList.map((country, index) => (
+          <div className="card bg-secondary" style={{ width: '18rem', border: '1px solid #ccc', margin: '10px', padding: '10px' }} key={index} >
+            <img src={country.flags.svg} alt={`${country.name.common} flag`} class="card-img-top" />
+            <ul className="list-group list-group-flush ">
+              <li className="list-group-item">Name: {country.name.common}</li>
+              <li className="list-group-item">Capital: {country.capital ? country.capital[0] : 'N/A'}</li>
+              <li className="list-group-item">Population: {country.population}</li>
+              <li className="list-group-item">Area: {country.area} km²</li>
+            </ul>
+          </div>
+        ))}
       </div>
-      )}
+
     </div>
   );
 };
